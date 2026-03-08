be8ac9335fe992fc395b83169cb194b7df2c6793
DachuanSong <songdc98@gmail.com>
2026-03-08 19:36:26 -0400

    Initial Commit

---
+++ HealthPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthPlanner
+//
+//  Created by Dachuan Song on 3/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthPlanner/HealthPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthPlannerApp.swift
+//  HealthPlanner
+//
+//  Created by Dachuan Song on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
